90e146a3d5064167e4ba2e378aae51e453300608
mykniz <nickshundeev@gmail.com>
2021-03-19 13:25:22 +0300

    AppEndpoint fixxed

--- src/main/java/ru/mykniz/endpoints/AppEndpoint.java
+++ src/main/java/ru/mykniz/endpoints/AppEndpoint.java
@@ -3,6 +3,8 @@ package ru.mykniz.endpoints;
 import lombok.Data;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +13,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 @Endpoint(id = "app-endpoint")
@@ -24,12 +27,12 @@ public class AppEndpoint {
 
     @PostConstruct
     public void init() {
-        appInfo.put("version 0.1",
+        appInfo.put("version-0.1",
                 Arrays.asList(this.name + " for localhost created",
                         "postgresql for localhost used",
                         "redis for localhost used",
                         "rabbitmq for localhost used"));
-        appInfo.put("version 0.2",
+        appInfo.put("version-0.2",
                 Arrays.asList(this.name + " for docker created",
                         "postgresql for docker used",
                         "redis for docker used",
@@ -37,8 +40,18 @@ public class AppEndpoint {
     }
 
     @ReadOperation
+    public List<String> getInfoByVersion(@Selector String version) {
+        return appInfo.get(version);
+    }
+
+    @ReadOperation
     public Map<String, List<String>> getAppInfo() {
         return appInfo;
+    }
+
+    @WriteOperation
+    public void setAppInfo(@Selector String version, String notes) {
+        appInfo.put(version, Arrays.stream(notes.split(",")).collect(Collectors.toList()));
     }
 
 }
